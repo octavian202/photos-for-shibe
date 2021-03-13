62e550526cc59239b5d521c9ce58fc8e92bc3a2e
Octavian <icaracioni7@gmail.com>
2021-03-13 14:15:10 +0200

    The photo urls are added

---
+++ index.js
@@ -0,0 +1,35 @@
+const PATH = "./db.json";
+
+function writeJsonToDb(path, data) {
+  try {
+    let dataJson = JSON.stringify(data);
+    Deno.writeTextFileSync(path, dataJson);
+    return `${dataJson}
+          Writen to ${path}
+          `;
+  } catch (err) {
+    return err.message;
+  }
+}
+
+async function getPhotos(count) {
+  let apiUrl =
+    `http://shibe.online/api/shibes?count=${count}&urls=true&httpsUrls=true`;
+  let response = await fetch(apiUrl);
+  let data = await response.json();
+  return data;
+}
+
+getPhotos(100)
+  .then((data) => {
+    console.log(data);
+    return {
+      urls: data,
+      profile: {
+        name: "typicode",
+      },
+    };
+  })
+  .then((photos) => {
+    console.log(writeJsonToDb("./db.json", photos));
+  });
